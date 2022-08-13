5d7eacb919f6adff4c6b8c1eedce525e4c9ca27a
nashjunior <nashjuniorjj@gmail.com>
2022-08-13 09:20:42 -0300

    feat: :label: add requests and response types

---
+++ src/interfaces/requests/ICreateRequest.ts
@@ -0,0 +1,3 @@
+export interface ICreateRequest {
+  criado_por: string;
+}
---
+++ src/interfaces/response/user/index.ts
@@ -0,0 +1,3 @@
+export * from './CreatedPersonType';
+export * from './CreatedWorkerType';
+export * from './CreatedClientType';
