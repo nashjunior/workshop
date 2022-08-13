bd8e88d0139eb7fed340a6a879f9df827033b092
nashjunior <nashjuniorjj@gmail.com>
2022-08-13 19:30:22 -0300

    feat: :art: add controller on subdomains clients

---
+++ src/domains/users/subdomains/client/controllers/clientController.ts
@@ -0,0 +1,28 @@
+import { ICreateRequest } from '@interfaces/requests';
+import { dependecyContainer } from 'container';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { CreateClientWokers } from '../useCases';
+
+type CreateRequestType = ICreateRequest & {
+  id_client: string;
+  ids_workers: string[];
+};
+
+export class ClientController {
+  async create(
+    request: FastifyRequest<{ Body: CreateRequestType }>,
+    response: FastifyReply,
+  ) {
+    const { id_client, ids_workers } = request.body;
+
+    const createClientWokers = dependecyContainer.resolve(CreateClientWokers);
+
+    const clientsWokers = await createClientWokers.execute({
+      idClient: id_client,
+      idsWorkers: ids_workers,
+      criado_por: 'aeuah',
+    });
+
+    return response.status(201).send(clientsWokers);
+  }
+}
