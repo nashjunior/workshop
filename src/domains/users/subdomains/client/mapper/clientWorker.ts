7b72210a1a6b51d8eb9c54cf96488ed86610b125
nashjunior <nashjuniorjj@gmail.com>
2022-08-13 19:23:57 -0300

    feat: :art: add mapper for client

---
+++ src/domains/users/subdomains/client/mapper/clientWorker.ts
@@ -0,0 +1,24 @@
+import { ClientWorker } from '@domains/users/entities';
+import { CreatedClientWorkerType } from '@interfaces/response';
+
+export const modelClientWorkerToApi = ({
+  id,
+  createdAt,
+  updatedAt,
+  worker: { id: uuidWorker },
+  client: { id: uuidClient },
+}: ClientWorker): CreatedClientWorkerType => {
+  return {
+    id,
+    id_client: uuidClient.trim(),
+    id_worker: uuidWorker.trim(),
+    created_at: createdAt,
+    updated_at: updatedAt,
+  };
+};
+
+export const manyModelClientWorkersToAPI = (
+  clients: ClientWorker[],
+): CreatedClientWorkerType[] => {
+  return clients.map(client => modelClientWorkerToApi(client));
+};
