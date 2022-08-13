12222af40b7b052edf6329dee43bb55f9d9dfb04
nashjunior <nashjuniorjj@gmail.com>
2022-08-13 19:31:55 -0300

    feat: :truck: route prefix /clients should use clients router

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
+import { clientsRouter } from '@domains/users/subdomains/client';
 import { userRouter } from '@domains/users/subdomains/user';
 import { server } from './app';
 
 server.register(userRouter, { prefix: '/users' });
+server.register(clientsRouter, { prefix: '/clients' });
