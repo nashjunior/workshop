3d71e06a7caae1f3d3b92e7ab80732cc781f489a
nashjunior <nashjuniorjj@gmail.com>
2022-08-17 20:39:04 -0300

    feat: :truck: add route find brand by uuid

--- src/domains/vehicles/subdomains/brand/routes/brands.ts
+++ src/domains/vehicles/subdomains/brand/routes/brands.ts
@@ -1,12 +1,15 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify';
 import { BrandsController } from '../controllers/brands';
 
+const brandsController = new BrandsController();
+
 export const brandsRouter = function (
   fastify: FastifyInstance,
   options: FastifyPluginOptions,
   done: (error?: Error) => void,
 ) {
-  fastify.post('/', new BrandsController().create);
+  fastify.post('/', brandsController.create);
+  fastify.get('/:id', brandsController.show);
 
   done();
 };
