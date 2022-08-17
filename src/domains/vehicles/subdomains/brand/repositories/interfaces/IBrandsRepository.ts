927a33b4ba5dd33d72e21db7574265a13cb07771
nashjunior <nashjuniorjj@gmail.com>
2022-08-17 19:43:39 -0300

    feat: :art: add brand interface repository

---
+++ src/domains/vehicles/subdomains/brand/repositories/interfaces/IBrandsRepository.ts
@@ -0,0 +1,7 @@
+import { Brand } from '@domains/vehicles/entites';
+import { CreateBrandDTOType } from '../../dtos';
+
+export interface IBrandsRepository {
+  findByName(name: string): Promise<Brand | null>;
+  createOne(dto: CreateBrandDTOType): Promise<Brand>;
+}
