b7072ad4458e52f6381e073ad57b388603a91540
nashjunior <nashjuniorjj@gmail.com>
2022-08-18 10:07:31 -0300

    feat: :art: add method list brands

--- src/domains/vehicles/subdomains/brand/controllers/brands.ts
+++ src/domains/vehicles/subdomains/brand/controllers/brands.ts
@@ -1,6 +1,7 @@
 import { dependecyContainer } from 'container';
 import { FastifyRequest, FastifyReply } from 'fastify';
-import { CreateBrandService } from '../useCases';
+import { IRequestQueryType } from '../../../../../interfaces/requests';
+import { CreateBrandService, FindBrandsService } from '../useCases';
 import { FindBrandByUUIDService } from '../useCases/findByUUID';
 
 type BodyType = { name: string };
@@ -35,5 +36,26 @@ export class BrandsController {
     const brand = await findBrandByUUIDService.execute(id);
 
     return response.send(brand);
+  }
+
+  async list(
+    request: FastifyRequest<{ Querystring: IRequestQueryType }>,
+    response: FastifyReply,
+  ) {
+    const { order_sort, query, query_fields, sort, deleted, page, perPage } =
+      request.query;
+
+    const findBrandsService = dependecyContainer.resolve(FindBrandsService);
+    const brands = await findBrandsService.execute({
+      deleted: Boolean(deleted),
+      queryFields: Array.isArray(query_fields) ? query_fields : [query_fields],
+      query,
+      sortedFields: sort as any[],
+      sortedFieldsType: order_sort,
+      page,
+      perPage,
+    });
+
+    return response.send(brands);
   }
 }
