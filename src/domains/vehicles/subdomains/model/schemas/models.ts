8b0a77e65534979a949d860642d7d3eea76310a7
nashjunior <nashjuniorjj@gmail.com>
2022-08-18 19:08:23 -0300

    feat: :art: add schema on find options

--- src/domains/vehicles/subdomains/model/schemas/models.ts
+++ src/domains/vehicles/subdomains/model/schemas/models.ts
@@ -1,6 +1,58 @@
 import * as Yup from 'yup';
+import { QueryFields, SortFieldsType } from '../enums';
 
 export const createModelSchema = Yup.object().shape({
   name: Yup.string().required().max(128),
   idBrand: Yup.string().uuid('id brand must be uuid').required(),
 });
+
+export const findModelsSchema = Yup.object().shape(
+  {
+    query: Yup.string().notRequired(),
+    queryFields: Yup.array().of(
+      Yup.mixed<keyof typeof QueryFields>().oneOf(
+        Object.keys(QueryFields) as any,
+      ),
+    ),
+
+    sortedFieldsType: Yup.array()
+      .of(
+        Yup.string()
+          .oneOf(['asc', 'desc', 'ASC', 'DESC'])
+          .uppercase()
+          .required(),
+      )
+      .test(
+        'isSameSize',
+        'fields to sort length and types of sort length does not match',
+        function (fields) {
+          const {
+            parent: { sortedFieldsType },
+          } = this;
+
+          return sortedFieldsType?.length === fields?.length;
+        },
+      ),
+    sortedFields: Yup.array()
+      .of(
+        Yup.mixed<keyof typeof SortFieldsType>()
+          .oneOf(Object.keys(SortFieldsType) as any)
+          .required(),
+      )
+      .test(
+        'isSameSize',
+        'types sort length and fields to sort length  does not match',
+        function (fields) {
+          const {
+            parent: { sortedFields },
+          } = this;
+
+          return sortedFields?.length === fields?.length;
+        },
+      ),
+
+    page: Yup.number().notRequired().typeError('Invalid format page'),
+    perPage: Yup.number().notRequired().typeError('Invalid format perPage'),
+  },
+  ['sortedFields', 'sortedFieldsType'] as any,
+);
