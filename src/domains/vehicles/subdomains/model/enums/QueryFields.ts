7458b6c5f92a2d4ac6e75e856b095bea3c3f48e1
nashjunior <nashjuniorjj@gmail.com>
2022-08-18 19:04:43 -0300

    feat: :art: add find options enum

---
+++ src/domains/vehicles/subdomains/model/enums/QueryFields.ts
@@ -0,0 +1,4 @@
+export const QueryFields = {
+  MODEL_NAME: 'models.name',
+  BRAND_NAME: 'brands.name',
+};
---
+++ src/domains/vehicles/subdomains/model/enums/SortFields.ts
@@ -0,0 +1,5 @@
+export const SortFieldsType = {
+  MODEL_NAME: 'models.name',
+  BRAND_NAME: 'brands.name',
+  CREATED_AT: 'models.createdAt',
+};
