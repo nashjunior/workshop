dc4b47120f45faeb0d514e964710195a3df90315
nashjunior <nashjuniorjj@gmail.com>
2022-08-18 19:05:53 -0300

    feat: :sparkles: models repository should implement find many

--- src/domains/vehicles/subdomains/model/repositories/typeorm/ModelsRepository.ts
+++ src/domains/vehicles/subdomains/model/repositories/typeorm/ModelsRepository.ts
@@ -2,6 +2,7 @@ import { GarageDataSource } from '@database/sources';
 import { Model } from '@domains/vehicles/entites';
 import { IModelsRepository } from '../interfaces';
 import { v4 as uuidV4 } from 'uuid';
+import { Brackets } from 'typeorm';
 
 export const ModelsTypeormRepository = GarageDataSource.getRepository(
   Model,
@@ -16,5 +17,37 @@ export const ModelsTypeormRepository = GarageDataSource.getRepository(
       ...dto,
     });
     return this.save(instance);
+  },
+
+  find({ deleted = false, order, query, page, perPage }) {
+    const queryBuilder = this.createQueryBuilder('models')
+      .innerJoinAndSelect('models.brand', 'brand')
+      .where(`models.deletedAt ${deleted ? 'IS NOT NULL' : 'IS NULL'}`);
+
+    if (query) {
+      const { fields, value } = query;
+      queryBuilder.andWhere(
+        new Brackets(subQb => {
+          fields.forEach(field =>
+            subQb.orWhere(`${field} ILIKE ('%${value}%')`),
+          );
+        }),
+      );
+    }
+
+    if (order) {
+      order.fields.forEach((field, index) => {
+        if (index === 0) queryBuilder.orderBy(field, order.type[index]);
+        else queryBuilder.addOrderBy(field, order.type[index]);
+      });
+    }
+
+    const hasPagination = page && perPage;
+
+    if (hasPagination) {
+      queryBuilder.skip(page * perPage - perPage).take(perPage);
+    }
+
+    return queryBuilder.getManyAndCount();
   },
 });
