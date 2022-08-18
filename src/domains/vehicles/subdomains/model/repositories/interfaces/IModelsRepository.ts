696b432cbe05b07200c6f0520a8b05d35f33e74a
nashjunior <nashjuniorjj@gmail.com>
2022-08-18 19:05:33 -0300

    feat: :art: add brand interface repository method findBrands

--- src/domains/vehicles/subdomains/model/repositories/interfaces/IModelsRepository.ts
+++ src/domains/vehicles/subdomains/model/repositories/interfaces/IModelsRepository.ts
@@ -1,8 +1,11 @@
 import { Model } from '@domains/vehicles/entites';
 import { ICreateModelDTOType } from '../../dtos';
+import { FindModelsType } from './FindModelsType';
 
 export interface IModelsRepository {
   createOne(dto: ICreateModelDTOType): Promise<Model>;
 
   findByUUID(id: string): Promise<Model>;
+
+  find(options: FindModelsType): Promise<[Model[], number]>;
 }
