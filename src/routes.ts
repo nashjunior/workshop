7635fd80139f8c94cff3d57cb6a0e860e4825625
nashjunior <nashjuniorjj@gmail.com>
2022-08-18 14:29:10 -0300

    feat: :truck: route models should use models router

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { clientsRouter } from '@domains/users/subdomains/client';
 import { userRouter } from '@domains/users/subdomains/user';
 import { brandsRouter } from '@domains/vehicles/subdomains/brand';
+import { modelsRouter } from '@domains/vehicles/subdomains/model';
 import { server } from './app';
 
 server.register(userRouter, { prefix: '/users' });
 server.register(clientsRouter, { prefix: '/clients' });
 server.register(brandsRouter, { prefix: '/brands' });
+server.register(modelsRouter, { prefix: '/models' });
