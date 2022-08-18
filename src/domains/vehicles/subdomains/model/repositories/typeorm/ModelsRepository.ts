b37651cef5debf9c909f9c4d2a30bdb13143fa61
nashjunior <nashjuniorjj@gmail.com>
2022-08-18 15:45:07 -0300

    feat: :sparkles: models repository should find by uuid

--- src/domains/vehicles/subdomains/model/repositories/typeorm/ModelsRepository.ts
+++ src/domains/vehicles/subdomains/model/repositories/typeorm/ModelsRepository.ts
@@ -6,6 +6,10 @@ import { v4 as uuidV4 } from 'uuid';
 export const ModelsTypeormRepository = GarageDataSource.getRepository(
   Model,
 ).extend<IModelsRepository>({
+  findByUUID(id) {
+    return this.findOneOrFail({ where: { id }, relations: ['brand'] });
+  },
+
   createOne(dto) {
     const instance = this.create({
       id: uuidV4(),
