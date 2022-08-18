f0301a29a9172bd91915644e3dec944f93da9808
nashjunior <nashjuniorjj@gmail.com>
2022-08-18 15:44:37 -0300

    feat: :art: add method find model by uuid

--- src/domains/vehicles/subdomains/model/repositories/interfaces/IModelsRepository.ts
+++ src/domains/vehicles/subdomains/model/repositories/interfaces/IModelsRepository.ts
@@ -3,4 +3,6 @@ import { ICreateModelDTOType } from '../../dtos';
 
 export interface IModelsRepository {
   createOne(dto: ICreateModelDTOType): Promise<Model>;
+
+  findByUUID(id: string): Promise<Model>;
 }
