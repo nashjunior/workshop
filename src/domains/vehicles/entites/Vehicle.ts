e3a3a3cff5beb41ed0fc30d01999bb29c454a412
nashjunior <nashjuniorjj@gmail.com>
2022-08-24 09:47:23 -0300

    feat: :art: add entity vehicles

---
+++ src/domains/vehicles/entites/Vehicle.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { BaseEntity } from './BaseEntity';
+import { Model } from './Model';
+
+@Entity('vehicles', { schema: 'vehicles' })
+export class Vehicle extends BaseEntity {
+  @PrimaryColumn({ name: 'uuid' })
+  id: string;
+
+  @PrimaryGeneratedColumn({ name: 'id_vehicle' })
+  idVehicle: number;
+
+  @Column({ name: 'id_model' })
+  idModel: number;
+
+  @Column({ name: 'fabrication_year' })
+  fabricationYear: number;
+
+  @Column({ name: 'model_year' })
+  modelYear: number;
+
+  @Column()
+  description: string;
+
+  @Column({ name: 'created_by' })
+  createdBy: string;
+
+  @ManyToOne(() => Model)
+  @JoinColumn({ name: 'id_model' })
+  model: Model;
+}
