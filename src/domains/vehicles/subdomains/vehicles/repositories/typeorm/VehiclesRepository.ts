064b79fb0630c68a1bf55eb9e3018dba926d9039
nashjunior <nashjuniorjj@gmail.com>
2022-09-04 15:24:08 -0300

    feat: :art: vehicles repository should implment find

--- src/domains/vehicles/subdomains/vehicles/repositories/typeorm/VehiclesRepository.ts
+++ src/domains/vehicles/subdomains/vehicles/repositories/typeorm/VehiclesRepository.ts
@@ -2,6 +2,7 @@ import { GarageDataSource } from '@database/sources';
 import { Vehicle } from '@domains/vehicles/entites';
 import { IVehiclesRepository } from '../interfaces/IVehiclesRepository';
 import { v4 as uuidV4 } from 'uuid';
+import { Brackets } from 'typeorm';
 
 export const VehiclesTypeormRepository = GarageDataSource.getRepository(
   Vehicle,
@@ -20,5 +21,41 @@ export const VehiclesTypeormRepository = GarageDataSource.getRepository(
       where: { id },
       relations: ['model', 'model.brand'],
     });
+  },
+
+  find({ deleted = false, order, query, page, perPage }) {
+    const queryBuilder = this.createQueryBuilder('vehicles')
+      .innerJoinAndSelect(
+        'vehicles.model',
+        'model',
+        `vehicles.deletedAt ${deleted ? 'IS NOT NULL' : 'IS NULL'}`,
+      )
+      .innerJoinAndSelect('model.brand', 'brand');
+
+    if (query) {
+      const { fields, value } = query;
+      queryBuilder.andWhere(
+        new Brackets(subQb => {
+          fields.forEach(field =>
+            subQb.orWhere(`${field} ILIKE ('%${value}%')`),
+          );
+        }),
+      );
+    }
+
+    if (order) {
+      order.fields.forEach((field, index) => {
+        if (index === 0) queryBuilder.orderBy(field, order.type[index]);
+        else queryBuilder.addOrderBy(field, order.type[index]);
+      });
+    }
+
+    const hasPagination = page && perPage;
+
+    if (hasPagination) {
+      queryBuilder.skip(page * perPage - perPage).take(perPage);
+    }
+
+    return queryBuilder.getManyAndCount();
   },
 });
