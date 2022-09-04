a1fa2a3a58f60c93618a99911bcb63e617e71e0f
nashjunior <nashjuniorjj@gmail.com>
2022-09-04 16:35:55 -0300

    feat: :sparkles: should register part router to main routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { clientsRouter } from '@domains/users/subdomains/client';
 import { userRouter } from '@domains/users/subdomains/user';
 import { brandsRouter } from '@domains/vehicles/subdomains/brand';
 import { modelsRouter } from '@domains/vehicles/subdomains/model';
+import { partsRouter } from '@domains/vehicles/subdomains/part';
 import { vehiclesRouter } from '@domains/vehicles/subdomains/vehicles';
 import { server } from './app';
 
@@ -9,4 +10,5 @@ server.register(userRouter, { prefix: '/users' });
 server.register(clientsRouter, { prefix: '/clients' });
 server.register(brandsRouter, { prefix: '/brands' });
 server.register(modelsRouter, { prefix: '/models' });
-server.register(vehiclesRouter, { prefix: 'vehicles' });
+server.register(vehiclesRouter, { prefix: '/vehicles' });
+server.register(partsRouter, { prefix: '/parts' });
