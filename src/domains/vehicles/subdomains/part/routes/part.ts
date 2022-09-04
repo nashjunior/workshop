91a033382ee0a5163f01bbfab3a7195dbd84b1d8
nashjunior <nashjuniorjj@gmail.com>
2022-09-04 16:35:37 -0300

    feat: :truck: add part router

---
+++ src/domains/vehicles/subdomains/part/routes/part.ts
@@ -0,0 +1,17 @@
+import { dependecyContainer } from 'container';
+import { FastifyInstance, FastifyPluginOptions } from 'fastify';
+import { PartsController } from '../controllers';
+
+const partsController = dependecyContainer.resolve(PartsController);
+
+export const partsRouter = function (
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions,
+  done: (error?: Error) => void,
+) {
+  fastify.post('/', partsController.create);
+  // fastify.get('/', modelsController.list);
+  // fastify.get('/:id', modelsController.show);
+
+  done();
+};
