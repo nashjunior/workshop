bc061b2465cc56869b0142a301a88a90c5fda872
nashjunior <nashjuniorjj@gmail.com>
2022-09-04 16:29:07 -0300

    feat: :card_file_box: add migration parts

--- src/database/migrations/1662316322645-CreatePartsTable.ts
+++ src/database/migrations/1662316322645-CreatePartsTable.ts
@@ -1,21 +1,17 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateTableBrands1660753048155
-  implements MigrationInterface
-{
+export class CreatePartsTable1662316322645 implements MigrationInterface {
   private table = new Table({
-    name: 'brands',
-    schema: 'public',
+    name: 'parts',
+    schema: 'vehicles',
     columns: [
       { name: 'uuid', type: 'char', length: '64', isUnique: true },
-      { name: 'id_brand', type: 'int', isPrimary: true, isGenerated: true },
-
+      { name: 'id_part', type: 'int', isPrimary: true, isGenerated: true },
       { name: 'name', type: 'varchar', length: '128' },
-      {
-        name: 'created_by',
-        type: 'varchar',
-        isNullable: false,
-      },
+      { name: 'description', type: 'varchar', isNullable: true },
+
+      { name: 'created_by', type: 'varchar', isNullable: false },
+
       {
         name: 'created_at',
         type: 'timestamp',
@@ -23,17 +19,13 @@ export default class CreateTableBrands1660753048155
         isNullable: false,
       },
 
-      {
-        name: 'updated_by',
-        type: 'varchar',
-        isNullable: true,
-      },
-
+      { name: 'updated_by', type: 'varchar', isNullable: true },
       { name: 'updated_at', type: 'timestamp', isNullable: true },
 
       { name: 'deleted_at', type: 'timestamp', isNullable: true },
     ],
   });
+
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(this.table);
   }
