f5c45fdcd017bb91d23f0e9f0fa994f8b2673361
nashjunior <nashjuniorjj@gmail.com>
2022-09-05 11:26:23 -0300

    feat: :art: add part photo repository
    add method create many

--- src/domains/vehicles/subdomains/part/repositories/index.ts
+++ src/domains/vehicles/subdomains/part/repositories/index.ts
@@ -1,9 +1,18 @@
 import { dependecyContainer } from '../../../../../container';
 import { IPartsRepository } from './interfaces';
-import { PartsRepository as PartsTypeormRepository } from './typeorm';
+import { IPartsPhotosRepository } from './interfaces';
+import {
+  PartsPhotosRepository as PartsPhotosTypeormRepository,
+  PartsRepository as PartsTypeormRepository,
+} from './typeorm';
 
 export * from './interfaces';
 
 dependecyContainer.register<IPartsRepository>('PartsTypeormRepository', {
   useValue: PartsTypeormRepository,
 });
+
+dependecyContainer.register<IPartsPhotosRepository>(
+  'PartsPhotosTypeormRepository',
+  { useValue: PartsPhotosTypeormRepository },
+);
