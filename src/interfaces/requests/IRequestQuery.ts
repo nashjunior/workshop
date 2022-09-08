c4e92ce687036b8a945816b367ab1b57145cd028
nashjunior <nashjuniorjj@gmail.com>
2022-09-07 21:10:59 -0300

    style: :label: update request query type

--- src/interfaces/requests/IRequestQuery.ts
+++ src/interfaces/requests/IRequestQuery.ts
@@ -1,8 +1,8 @@
 export type IRequestQueryType = {
   query: string;
   'query_fields[]': string[];
-  sort: string[];
-  order_sort: ('ASC' | 'DESC')[];
+  'sort[]': string[];
+  'order_sort[]': ('ASC' | 'DESC')[];
   deleted?: string;
   page?: number;
   perPage?: number;
