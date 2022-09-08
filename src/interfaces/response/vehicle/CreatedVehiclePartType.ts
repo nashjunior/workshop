d4e15092abc9124c626045291d3d9aaf1eb91e2e
nashjunior <nashjuniorjj@gmail.com>
2022-09-08 09:53:54 -0300

    style: :label: add create vehicle type

---
+++ src/interfaces/response/vehicle/CreatedVehiclePartType.ts
@@ -0,0 +1,6 @@
+export type CreatedVehiclePartsType = {
+  id: string;
+  created_at: Date;
+  updated_at?: Date;
+  deleted_at?: Date;
+};
--- src/interfaces/response/vehicle/index.ts
+++ src/interfaces/response/vehicle/index.ts
@@ -2,3 +2,5 @@ export * from './CreatedBrandType';
 export * from './CreatedModelType';
 export * from './CreatedVehicleType';
 export * from './CreatedPartType';
+export * from './CreatedPartPhotoType';
+export * from './CreatedVehiclePartType';
