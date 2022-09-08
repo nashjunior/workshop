c60a13ba6344d04124fec276f67d6dbe6be7bccb
nashjunior <nashjuniorjj@gmail.com>
2022-09-07 21:15:37 -0300

    feat: :art: add part controller method
    should implement method that find parts

--- src/domains/vehicles/subdomains/part/controllers/Part.ts
+++ src/domains/vehicles/subdomains/part/controllers/Part.ts
@@ -1,9 +1,17 @@
 import { dependecyContainer } from '../../../../../container';
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { CreatePartService, FindPartByUUIDService } from '../useCases';
+import {
+  CreatePartService,
+  FindPartByUUIDService,
+  FindPartsService,
+} from '../useCases';
 import { injectable } from 'tsyringe';
+import { IRequestQueryType } from '../../../../../interfaces/requests';
 
-type BodyType = { name: string; description?: string };
+type BodyType = {
+  name: string;
+  description?: string;
+};
 
 @injectable()
 export class PartsController {
@@ -35,5 +43,42 @@ export class PartsController {
     const part = await findPartByUUIDService.execute(id);
 
     return response.send(part);
+  }
+
+  async list(
+    request: FastifyRequest<{ Querystring: IRequestQueryType }>,
+    response: FastifyReply,
+  ) {
+    const {
+      'order_sort[]': order_sort,
+      query,
+      'sort[]': sort,
+      deleted,
+      page,
+      perPage,
+      'query_fields[]': queryFields,
+    } = request.query;
+
+    let sortFields: string[] = [];
+    let orderSort: ('ASC' | 'DESC')[] = [];
+
+    if (Array.isArray(sort)) sortFields = sort;
+    else if (sort) sortFields.push(sort);
+
+    if (Array.isArray(order_sort)) orderSort = order_sort;
+    else if (sort) sortFields.push(order_sort);
+
+    const findPartsService = dependecyContainer.resolve(FindPartsService);
+    const parts = await findPartsService.execute({
+      deleted: Boolean(deleted),
+      queryFields: Array.isArray(queryFields) ? queryFields : [queryFields],
+      query,
+      sortedFields: sortFields,
+      sortedFieldsType: orderSort,
+      page,
+      perPage,
+    });
+
+    return response.send(parts);
   }
 }
