b5e3ecabfad1c51197726476592aec753cca6abd
nashjunior <nashjuniorjj@gmail.com>
2022-09-08 09:54:22 -0300

    refactor: :recycle: add import entity vehicle typoe

--- src/domains/vehicles/entites/index.ts
+++ src/domains/vehicles/entites/index.ts
@@ -3,11 +3,13 @@ import { Model } from './Model';
 import { Part } from './Part';
 import { PartPhoto } from './PartPhoto';
 import { Vehicle } from './Vehicle';
+import { VehiclePart } from './VehiclePart';
 
 export * from './Brand';
 export * from './Model';
 export * from './Vehicle';
 export * from './Part';
 export * from './PartPhoto';
+export * from './VehiclePart';
 
-export const entities = [Brand, Model, Part, PartPhoto, Vehicle];
+export const entities = [Brand, Model, Part, PartPhoto, Vehicle, VehiclePart];
