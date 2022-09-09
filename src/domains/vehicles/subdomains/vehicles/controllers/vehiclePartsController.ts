2522a9af5f4e6e883a04efb050496f11c07a5db9
nashjunior <nashjuniorjj@gmail.com>
2022-09-08 21:42:39 -0300

    feat: :art: add vehicle parts controller method
    should list all by id vehicle

--- src/domains/vehicles/subdomains/vehicles/controllers/vehiclePartsController.ts
+++ src/domains/vehicles/subdomains/vehicles/controllers/vehiclePartsController.ts
@@ -1,7 +1,11 @@
 import { dependecyContainer } from '../../../../../container';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { injectable } from 'tsyringe';
-import { CreateVehiclePartsService } from '../useCases';
+import {
+  CreateVehiclePartsService,
+  FindVehiclePartsByIdVehicleService,
+} from '../useCases';
+import { IRequestQueryType } from 'interfaces/requests';
 
 type BodyType = { id_vehicle: string; ids_parts: string[] };
 
@@ -24,5 +28,47 @@ export class VehiclePartsController {
     });
 
     return response.status(201).send(vehicle);
+  }
+
+  async listByIdVehicle(
+    {
+      params: { id },
+      query: {
+        query,
+        page,
+        perPage,
+        'query_fields[]': queryFields,
+        'order_sort[]': orderSort,
+        'sort[]': sort,
+      },
+    }: FastifyRequest<{
+      Params: { id: string };
+      Querystring: IRequestQueryType;
+    }>,
+    response: FastifyReply,
+  ) {
+    let sortedFields: string[] = [];
+    const sortedFieldsType: string[] = [];
+
+    if (Array.isArray(sort)) sortedFields = sort;
+    else if (sort) sortedFields.push(sort);
+
+    if (Array.isArray(orderSort)) sortedFields = orderSort;
+    else if (orderSort) sortedFields.push(orderSort);
+
+    const findVehiclePartsByIdVehicleService = dependecyContainer.resolve(
+      FindVehiclePartsByIdVehicleService,
+    );
+    const vehicleParts = await findVehiclePartsByIdVehicleService.execute({
+      queryFields: Array.isArray(queryFields) ? queryFields : [queryFields],
+      query,
+      sortedFields,
+      sortedFieldsType,
+      page,
+      perPage,
+      id,
+    });
+
+    return response.send(vehicleParts);
   }
 }
