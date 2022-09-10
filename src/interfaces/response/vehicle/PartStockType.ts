03340048cd50fa853e123580104e74c5d6662802
nashjunior <nashjuniorjj@gmail.com>
2022-09-09 21:39:42 -0300

    style: :label: add part stock type

---
+++ src/interfaces/response/vehicle/PartStockType.ts
@@ -0,0 +1,9 @@
+export type PartStockType = {
+  id: string;
+  amount: number;
+  unit_value: number;
+  default_selling_price: number;
+  created_at: Date;
+  updated_at?: Date;
+  deleted_at?: Date;
+};
