17df46e25667af4262020d88d24847b32c933aa5
nashjunior <nashjuniorjj@gmail.com>
2022-09-09 21:35:50 -0300

    refactor: :fire: remove useless variables and imports

--- src/domains/vehicles/entites/index.ts
+++ src/domains/vehicles/entites/index.ts
@@ -1,11 +1,3 @@
-import { Brand } from './Brand';
-import { Model } from './Model';
-import { Part } from './Part';
-import { PartPhoto } from './PartPhoto';
-import { Vehicle } from './Vehicle';
-import { VehiclePart } from './VehiclePart';
-import { VVehiclePart } from './VVehiclePart';
-
 export * from './Brand';
 export * from './Model';
 export * from './Vehicle';
@@ -13,13 +5,3 @@ export * from './Part';
 export * from './PartPhoto';
 export * from './VehiclePart';
 export * from './VVehiclePart';
-
-export const entities = [
-  Brand,
-  Model,
-  Part,
-  PartPhoto,
-  Vehicle,
-  VehiclePart,
-  VVehiclePart,
-];
